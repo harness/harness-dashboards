9ff71e6344f461f277a0319d0900a307288969c5
Manish Jaiswal <manish.jaiswal2@harness.io>
2023-11-09 14:49:31 +0530

    Restructuring CD Dashboards and folders

--- scripts/gsutil_script.sh
+++ scripts/gsutil_script.sh
@@ -19,14 +19,14 @@ gsutil_cp_module() {
 # Building per module may not be needed currently as each module just has file groups for now.
 # Having this in place if more bazel targets are added in the modules.
 gsutil_cp_module CCM
-gsutil_cp_module CD
+gsutil_cp_module CD-Core
 gsutil_cp_module CIE
 gsutil_cp_module Chaos
 gsutil_cp_module Cloud_Monitoring_SaaS_Only
 gsutil_cp_module Cluster_Pod_Details_SaaS_Only
 gsutil_cp_module Database
 gsutil_cp_module Feature_Flags
-gsutil_cp_module Gitops
+gsutil_cp_module CD-OPA-GitOps-CV
 gsutil_cp_module IAC
 gsutil_cp_module Monitoring_System
 gsutil_cp_module Platform
@@ -35,6 +35,6 @@ gsutil_cp_module STO
 gsutil_cp_module System_Health
 gsutil_cp_module System_Usability 
 gsutil_cp_module Continuous_ET
-gsutil_cp_module Pipeline_Service
+gsutil_cp_module CD-Pipeline
 gsutil_cp_module Weaviate
 gsutil_cp_module SRM
